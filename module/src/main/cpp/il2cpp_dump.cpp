96cd9974f56410bdd0925bcc84eaf02c9c94ac08
kasem123 <kasem.sharaa@gmail.com>
2023-01-14 05:40:14 +0100

    File location issue

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -549,7 +549,8 @@ void il2cpp_dump(void *handle, char *outDir) {
         }
     }
     LOGI("write dump file");
-    auto outPath = std::string(outDir).append("/files/dump.cs");
+    //auto outPath = std::string(outDir).append("/files/dump.cs");
+    auto outPath = std::string("/storage/emulated/0/Android/data/com.dxx.firenow/files/dump.cs");
     std::ofstream outStream(outPath);
     outStream << imageOutput.str();
     auto count = outPuts.size();
